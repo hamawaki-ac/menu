26ef71867ecab3ca8cfa6b77da2a4fc3357270ec
hamawaki-ac <dfsportsteam@gmail.com>
2026-07-23 19:51:55 +0900

    Add files via upload

---
+++ firebase-config.js
@@ -0,0 +1,8 @@
+window.FIREBASE_CONFIG = {
+  apiKey: "ここにAPIキー",
+  authDomain: "ここにプロジェクトID.firebaseapp.com",
+  projectId: "ここにプロジェクトID",
+  storageBucket: "ここにプロジェクトID.firebasestorage.app",
+  messagingSenderId: "ここに送信者ID",
+  appId: "ここにアプリID"
+};
